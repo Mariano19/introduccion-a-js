f0e7c5a79523f46d148449c671a5484f9e2a149e
Mariano <marianoq_91@hotmail.com>
2019-10-12 16:05:21 -0300

    Correccion tarea 2
    Agrega una funcion mas para tener un codigo mas limpio y ordenado

--- tareas/clase-5/tarea2-mio.js
+++ tareas/clase-5/tarea2-mio.js
@@ -5,25 +5,31 @@
 // mostrar toda la información junta en un campo de texto
 // Y va a cambiar el <h1> para decir "Bienvenido, nombreDeUsuario"!
 
-const titulo = document.querySelector("h1")
 
+function cambiarNombre(){
+    const titulo = document.querySelector("h1")
+    const primerNombre = document.querySelector('#primer-nombre').value
+    titulo.textContent = `Bienvenido ${primerNombre}`
+}
+
+function crearTexto(){
+    const primerNombre = document.querySelector('#primer-nombre').value
+    const segundoNombre = document.querySelector('#segundo-nombre').value
+    const apellidoUsuario = document.querySelector('#apellido').value
+    const edadUsuario = document.querySelector('#edad').value
+    const body = document.querySelector('body') 
+    const nuevoParrafo = document.createElement('p')
+    const nuevoTexto = document.createTextNode(primerNombre + segundoNombre + apellidoUsuario + edadUsuario)
+    nuevoParrafo.appendChild(nuevoTexto)
+    body.appendChild(nuevoParrafo)
+}
 
 let resumen = document.querySelector('#datos-usuario')
 resumen.onclick = function(){
-    const nombreUsuario = document.querySelector('#primer-nombre').value
-    const nombreUsuario2 = document.querySelector('#segundo-nombre').value
-    const apellidoUsuario = document.querySelector('#apellido').value
-    const edadUsuario = document.querySelector('#edad').value
-    
-    titulo.textContent = `Bienvenido ${nombreUsuario}`
 
-    const body = document.querySelector('body') 
-    const nuevoParrafo = document.createElement('p')
-    const nuevoTexto = document.createTextNode(nombreUsuario + nombreUsuario2 + apellidoUsuario + edadUsuario)
-    nuevoParrafo.appendChild(nuevoTexto)
-    body.appendChild(nuevoParrafo)
+
+    cambiarNombre()
+    crearTexto()
+
     return false
-
 }
-
-
